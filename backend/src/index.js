d5c7ad4365be8754ddba109790997b188025c3f1
jotlove <76457611+jotlove@users.noreply.github.com>
2025-12-28 11:17:16 -0800

    Fix formatting and improve production route handling

--- backend/src/index.js
+++ backend/src/index.js
@@ -26,10 +26,10 @@ app.use(cors({
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
 
-if(process.env.NODE_ENV==="production"){
+if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("/*", (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
